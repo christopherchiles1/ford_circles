11e9f14e55f53c76e59e062cf8cc41a5287bde5e
Chris Chiles <christopher.chiles1@gmail.com>
2019-05-11 13:56:37 -0700

    use greyscale coloring based on factorability

--- fractal.js
+++ fractal.js
@@ -1,15 +1,15 @@
 function setup() {
   let resolution = 380;
-  createCanvas(4 * resolution, resolution);
+
+  createCanvas(4 * resolution, resolution + 25);
   depthSlider = createSlider(2, 30, 5);
   depthSlider.position(20, 20);
 
-  stroke(255);
-  noFill();
+  stroke(0);
 }
 
 function draw() {
-  background(0);
+  background(255);
 
   let depth = depthSlider.value();
   textSize(16);
@@ -25,19 +25,20 @@ function draw() {
 function drawFareySequence(start, end, depth) {
   let middle = start.fareyAdd(end);
 
-  let r = map(middle.denominator, 2, 30, 150, 0);
-  let g = map(middle.denominator, 2, 30, 0, 255);
-  let b = map(middle.denominator, 2, 30, 0, 75);
-  fill(r, g, b);
-  drawFareyCircle(middle);
-  noFill();
-  if (middle.denominator === depth) {
-    textAlign(CENTER);
-    text(middle.toString(), map(middle.toFloat(), 0, 1, 0, width), height - 8)
-    textAlign(LEFT);
-  }
+  if (middle.denominator <= depth) {
+    let numFactors = factorize(middle.denominator).length - 2;
+    let factorability = numFactors / parseFloat(middle.denominator);
+    let grayscale = map(factorability, 0, 0.5, 200, 50);
+    fill(grayscale);
 
-  if (middle.denominator < depth) {
+    drawFareyCircle(middle);
+    noFill();
+    if (middle.denominator === depth) {
+      textAlign(CENTER);
+      text(middle.toString(), map(middle.toFloat(), 0, 1, 0, width), height - 8)
+      textAlign(LEFT);
+    }
+
     drawFareySequence(start, middle, depth);
     drawFareySequence(middle, end, depth);
   }
@@ -50,6 +51,16 @@ function drawFareyCircle(fraction) {
   let x = map(floatVal, 0, 1, 0, width);
   let r = map(radius, 0, 1, 0, width);
   circle(x, height - r - 25, r);
+}
+
+  function factorize(number) {
+  let factors = [];
+  for (let i = 1; i <= number; i++) {
+    if (number % i === 0) {
+      factors.push(i)
+    }
+  }
+  return factors;
 }
 
 // Inspirations
